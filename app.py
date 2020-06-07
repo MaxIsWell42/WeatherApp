1456d3dd4801e04f69c783e4f36b8d40995dae83
HexSeal <maxfin13@gmail.com>
2020-06-07 17:25:08 -0400

    Smoothed everything out, just need to fix a few last bugs

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
+import os
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def homepage():
+    if request.method == "POST":
+        city = request.form("City Name: ")
+        state = request.form("State Code: ")
+    
     params = {
-    'api_key': '{API_KEY}',
+    'api_key': os.environ.get('API_KEY'),
+    'city_name': city,
+    'state_code': state,
     }
+    
     r = requests.get(
-        'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data',
+        'api.openweathermap.org/data/2.5/weather?q={city_name},{state_code}&appid={API_KEY}',
         params=params)
-    return render_template('weatherHome.html', weather=json.loads(r.text)['']) # Add name of JSON catagory we're looking for
+    print(r.url, "hello")
+    
+    return render_template('weatherHome.html', weather=r) # Add name of JSON catagory we're looking for
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
