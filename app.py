d2753f07d55bcc1e302df45d96716c309b9d6aac
HexSeal <maxfin13@gmail.com>
2020-06-09 02:06:21 -0400

    [add] Have forms working and API is working and returning JSON, which I return part of. Also now has error messages and form validation. All I need is to pass form data to the next page

--- app.py
+++ app.py
@@ -1,38 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+from forms import LocationForm
 import requests
 import json
 import os
 
+# Start the Flask app
 app = Flask(__name__)
 
-@app.route('/', methods=["GET","POST"])
+# Generate a CSRF Secret Key
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+@app.route('/success', methods=["GET","POST"])
 def homepage():
     # Get the API key
     api_key = os.environ.get('API_KEY')
+    # print(api_key)
     
     # Some default values
     city = "San Francisco"
-    state = "CA"
-    
-    # If a location has not been submitted yet, ask for one(WIP)
-    if request.method == "POST":
-        city = request.form("City Name: ")
-        state = request.form("State Code: ")
-    
-    # params = {
-    # 'city_name': city,
-    # 'state_code': state,
-    # 'api_key': os.environ.get('API_KEY'),
-    # }
-    # print(params['api_key'])
-    
+    state = "California"
+        
     # Make the request
     url = 'http://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}'
     r = requests.get(
-        url.format(city, state, api_key)).json()
-    # print("\n", r.url, "\n")
+        url.format(city, state, api_key))
+    # x = json.loads(r)
+    # r = x["weather"]
+    weatherJSON = r.json()
+    r = weatherJSON["weather"]
+    # print("\n", r.json(), "\n")
     
     return render_template('base.html', weather=r) # Add name of specific JSON category if you want one
+
+@app.route("/", methods = ['GET', 'POST'])
+def Location():
+    form = LocationForm()
+    if form.validate_on_submit():
+        city = LocationForm.city.data
+        state = LocationForm.state.data
+        print(city, state)
+        return url_for('success')
+    return render_template('location.html', form=form)
 
 if __name__ == '__main__':
     app.run()
--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
 
-class SubmitLocationForm(Form):
+class LocationForm(Form):
     city    = StringField('City', [validators.Length(min=4, max=25)])
     state   = StringField('State', [validators.Length(min=4, max=25)])
     mood    = StringField('Mood', [validators.Length(min=3, max=34)])
+    submit = SubmitField('Enter')
